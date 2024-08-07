f136778a6db9643a0aab081e8843bb6011dc7f09
geoffreyvanwyk <geoffreyvanwyk@users.noreply.github.com>
2024-08-07 10:10:30 +0000

    refactor: fix coding style

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -23,7 +23,7 @@
  * @license    {@link http://www.gnu.org/copyleft/gpl.html} GNU GPL v3 or later
  */
 
- namespace Spoorsny\Laravel\Tests;
+namespace Spoorsny\Laravel\Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
